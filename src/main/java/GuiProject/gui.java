9cac8a39ec58a9f3c465c2d054574f2917d3db35
vkamunya <vanessawanjirundumia@gmail.com>
2021-06-16 14:12:53 +0300

    third commit

--- src/main/java/GuiProject/gui.java
+++ src/main/java/GuiProject/gui.java
@@ -3,70 +3,80 @@ import net.proteanit.sql.DbUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
 import java.sql.*;
 import java.io.FileWriter;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 
-public class gui extends javax.swing.JFrame{
+public class gui extends javax.swing.JFrame {
 
     private JButton Login;
     private JPanel panel1;
-     JTextField username;
-     JTextField password;
+    JTextField studentid;
+    JTextField studentname;
     private JButton saveButton;
     private JTable table1;
+    private JList list1;
+    private JButton addStudentButton;
+    private JButton updateButton;
+    private JTextField course;
+    private JTextField mark1;
+    private JTextField mark2;
+    private JButton addList;
+    private JButton addtable;
+    Connection conn = null;
     PreparedStatement pst = null;
     ResultSet rs = null;
-
 
 
     public gui() {
         Login.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String name= username.getText();
-                String pass= password.getText();
+                String id = studentid.getText();
+                String name = studentname.getText();
+                String major = course.getText();
+                String marks1 = mark1.getText();
+                String marks2 = mark2.getText();
                 String host = "localhost";
                 String port = "5432";
-                String db_name = "test";
+                String db_name = "student";
                 String username = "postgres";
                 String password = "admin";
 
 
+                try {
+                    Class.forName("org.postgresql.Driver");
+                    Connection conn = DriverManager.getConnection(
+                            "jdbc:postgresql://" + host + ":" + port + "/" + db_name + "", "" + username + "", "" + password + "");
 
-                    try {
-                        Class.forName("org.postgresql.Driver");
-                        Connection conn = DriverManager.getConnection(
-                                "jdbc:postgresql://"+host+":"+port+"/"+db_name+"",""+username+"",""+password+"");
-
-                        if (conn != null) {
-                            String sql = "insert into test values (' "+name+"',' "+pass+"')";
-                            Statement statement= conn.createStatement();
-                            int x= statement.executeUpdate(sql);
-                            if (x==0){
-                                JOptionPane.showMessageDialog(Login,"User Exists");
-                            }
-                            else {
-                                JOptionPane.showMessageDialog(Login,"Login Successful");
-                            }
-                            conn.close();
+                    if (conn != null) {
+                        String sql = "insert into student values (' " + id + "',' " + name + "', ' " + major + "',' " + marks1 + "')";
+                        Statement statement = conn.createStatement();
+                        int x = statement.executeUpdate(sql);
+                        if (x == 0) {
+                            JOptionPane.showMessageDialog(Login, "User Exists");
+                        } else {
+                            JOptionPane.showMessageDialog(Login, "Login Successful");
+                        }
+                        conn.close();
 
 
                     }
-                    } catch (Exception e1) {
-                        e1.printStackTrace();
-                    }
-
-
-
+                } catch (Exception e1) {
+                    e1.printStackTrace();
                 }
 
 
-            });
+            }
+
+
+        });
 
 
         saveButton.addActionListener(new ActionListener() {
@@ -74,53 +84,97 @@ public class gui extends javax.swing.JFrame{
             public void actionPerformed(ActionEvent e) {
                 JSONObject obj = new JSONObject();
                 JSONArray jrr = new JSONArray();
-                obj.put("Username",username.getText());
-                obj.put("Password",password.getText());
+                obj.put("Username", studentid.getText());
+                obj.put("Password", studentname.getText());
                 jrr.add(obj);
-                JOptionPane.showMessageDialog(null,obj);
+                JOptionPane.showMessageDialog(null, obj);
                 try {
-                    FileWriter file = new FileWriter("Userdata.json");
+                    FileWriter file = new FileWriter("student.json");
                     file.write(jrr.toJSONString());
                     file.close();
+                } catch (Exception e2) {
+                    JOptionPane.showMessageDialog(null, "Error occurred");
                 }
-                catch (Exception e2){
-                    JOptionPane.showMessageDialog(null,"Error occurred");
-                }
-                JOptionPane.showMessageDialog(null,"Data Saved");
+                JOptionPane.showMessageDialog(null, "Data Saved");
 
-                showTableData();
+
 
             }
 
         });
-    }
-    public void showTableData(){
-        try{
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/test)",""+username+"",""+password+"");
-            String sql = "select * from test";
-            pst = conn.prepareStatement(sql);
-            rs=pst.executeQuery();
-            table1.setModel(DbUtils.resultSetToTableModel(rs));
-            conn.close();
-        }
-
-catch(Exception ex){
-            JOptionPane.showMessageDialog(null, ex);
-        }
-
-    }
+        addList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Class.forName("org.postgresql.Driver");
+                    Connection conn = DriverManager.getConnection(
+                            "jdbc:postgresql://localhost:5432/student)", "" + studentid + "", "" + studentname + "");
+                    String sql = "select * from student";
+                    pst = conn.prepareStatement(sql);
+                    rs = pst.executeQuery();
+                    table1.setModel(DbUtils.resultSetToTableModel(rs));
 
 
+                    while (rs.next()) {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Successful");
-        frame.setContentPane(new gui().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+                        String student_id = rs.getString("id");
+                        String student_name = rs.getString("name");
+                        String major = rs.getString("major");
+                        String marks1 = rs.getString("marks1");
+                        String marks2 = rs.getString("marks2");
+
+
+
+                        String tbData[] = {student_id, student_name, major, marks1, marks2};
+                        DefaultTableModel tblModel = (DefaultTableModel) table1.getModel();
+
+                        tblModel.addRow(tbData);
+                    }
+
+                    conn.close();
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, ex);
+                }
+            }
+        });
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+
+                    String sql = "UPDATE student SET name=?,major=?, marks1=?, marks2=? WHERE id=?";
+                    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/student", "root", "");
+                    pst = conn.prepareStatement(sql);
+                    pst.setString(1, studentid.getText());
+                    pst.setString(2, studentname.getText());
+                    pst.setString(3, course.getText());
+                    pst.setString(4, mark1.getText());
+                    pst.setString(5, mark2.getText());
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "updated successfully");
+
+                } catch (SQLException | HeadlessException ex) {
+                    JOptionPane.showMessageDialog(null, ex);
+                }
+
 
             }
 
+
+        });
+    }
+
+
+
+        public static void main (String[]args){
+            JFrame frame = new JFrame("Successful");
+            frame.setContentPane(new gui().panel1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+
+
         }
+
+
+}
